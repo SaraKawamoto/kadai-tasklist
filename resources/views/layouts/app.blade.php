0815f99c75cd27f73e7db49d6c91778380a88885
sara.kawamoto <sara.kawamoto@rakuten.com>
2018-06-06 05:42:11 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
         @include('commons.navbar')
         
         <div class="container">
-          
+           @include('commons.error_messages')
             
             @yield('content')
             </div>
